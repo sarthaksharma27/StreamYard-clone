a6c96d12ff09ad607ddef203dd3517553d4fdbaf
sarthaksharma27 <sharma.sarthak222872@gmail.com>
2023-10-20 20:33:39 +0530

    Change the format to  H.264 format

--- public/js/main.js
+++ public/js/main.js
@@ -1,81 +1,99 @@
 let localStream;
-let canvas = document.getElementById('canvas');
+const canvas = document.getElementById('canvas');
+let mediaRecorder; // Add a variable for MediaRecorder
+const chunks = [];
 
-let init = async () => {
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true }); 
-  document.getElementById('user').srcObject = localStream;
+async function init() {
+  try {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    document.getElementById('user').srcObject = localStream;
 
-  let ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
 
-  function captureAndDrawFrame() {
-    ctx.drawImage(document.getElementById('user'), 0, 0, canvas.width, canvas.height);
-    requestAnimationFrame(captureAndDrawFrame);
+    function captureAndDrawFrame() {
+      ctx.drawImage(document.getElementById('user'), 0, 0, canvas.width, canvas.height);
+      requestAnimationFrame(captureAndDrawFrame);
+    }
+
+    captureAndDrawFrame();
+
+    // Initialize the MediaRecorder with H.264 codec
+    mediaRecorder = new MediaRecorder(canvas.captureStream(), {
+      mimeType: 'video/webm;codecs=h264',
+    });
+
+    // Handle data available from MediaRecorder
+    mediaRecorder.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        chunks.push(event.data);
+      }
+    };
+
+    initializeWebRTC();
+  } catch (error) {
+    console.error('Error initializing media stream:', error);
   }
-
-  captureAndDrawFrame();
-
-  initializeWebRTC();
-};
+}
 
 function initializeWebRTC() {
   const configuration = { iceServers: [{ urls: 'stun:stun1.l.google.com:19302' }] };
   const peerConnection = new RTCPeerConnection(configuration);
 
   const canvasStream = canvas.captureStream();
-  canvasStream.getTracks().forEach((track) => {
-    peerConnection.addTrack(track, canvasStream);
-  });
+  canvasStream.getTracks().forEach(track => peerConnection.addTrack(track, canvasStream));
 
-  const dataChannel = peerConnection.createDataChannel('video'); 
+  const dataChannel = peerConnection.createDataChannel('video');
 
-  dataChannel.onopen = () => {
-    sendDataFrames(dataChannel);
-  };
+  dataChannel.onopen = () => sendDataFrames(dataChannel);
 
   peerConnection.createOffer()
-    .then((offer) => {
-      return peerConnection.setLocalDescription(offer);
-    })
+    .then(offer => peerConnection.setLocalDescription(offer))
     .then(() => {
       const offerMessage = {
         type: 'offer',
         sdp: peerConnection.localDescription.sdp,
       };
 
-      
       console.log('Offer Message:', offerMessage);
 
-      // Send the offer to your backend via WebSocket
       const websocket = new WebSocket('ws://localhost:8080');
-      websocket.onopen = () => {
-        websocket.send(JSON.stringify(offerMessage));
-      };
-
-      websocket.onerror = (error) => {
-        console.error('WebSocket error:', error);
-      };
+      websocket.onopen = () => websocket.send(JSON.stringify(offerMessage));
+      websocket.onerror = error => console.error('WebSocket error:', error);
     })
-    .catch((error) => {
-      console.error('Error creating offer:', error);
-    });
+    .catch(error => console.error('Error creating offer:', error));
 }
 
 function sendDataFrames(dataChannel) {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+  mediaRecorder.start();
 
-  function captureAndSendFrame() {
-    ctx.drawImage(document.getElementById('user'), 0, 0, canvas.width, canvas.height);
-    const frameData = canvas.toDataURL('image/jpeg');
-    dataChannel.send(frameData);
-    requestAnimationFrame(captureAndSendFrame);
-  }
+  // This function will handle the data available event and push the data to the 'chunks' array.
+  mediaRecorder.ondataavailable = (event) => {
+    if (event.data.size > 0) {
+      chunks.push(event.data);
+    }
+  };
+}
 
-  captureAndSendFrame();
+// When you want to stop recording and send the video to the backend
+function stopRecordingAndSend() {
+  mediaRecorder.stop();
+
+  // Convert 'chunks' to a Blob
+  const h264VideoBlob = new Blob(chunks, { type: 'video/webm;codecs=h264' });
+
+  // Send the Blob to your backend for further processing by FFmpeg
+  sendToBackend(h264VideoBlob);
+}
+
+// Add event listener for stopping the recording and sending
+document.getElementById('stop-btn').addEventListener('click', stopRecordingAndSend);
+
+// You may need to implement the 'sendToBackend' function to send the Blob to your backend.
+function sendToBackend(blob) {
+  // Implement the logic to send the Blob to your backend (e.g., via AJAX or WebSocket).
 }
 
 init();
-
 
 let toggleCamera = async () => {
   let videoTrack = localStream.getTracks().find(track => track.kind === 'video')
--- webrtcRoute.js
+++ webrtcRoute.js
@@ -31,41 +31,73 @@ wss.on('connection', (ws) => {
     peerConnection.setRemoteDescription(new RTCSessionDescription(offer))
       .then(() => {
         console.log('Remote description set successfully.');
-
-        ws.on('message', (data) => {
-          videoFrames.push(data); 
-          if (videoFrames.length > 0) {
-            setTimeout(() => {
-              decodeAndStreamToYouTube();
-            }, 5000); // 5 seconds
-          }
-        });
       })
       .catch((error) => {
         console.error('Error setting remote description:', error);
       });
   });
+
+  ws.on('message', (data) => {
+    console.log('Received video frame');
+    videoFrames.push(data);
+    if (videoFrames.length > 0) {
+      setTimeout(() => {
+        decodeAndStreamToYouTubeAndTwitch();
+      }, 5000);
+    }
+  });
 });
 
-function decodeAndStreamToYouTube() {
+function decodeAndStreamToYouTubeAndTwitch() {
   const input = new ffmpeg();
-  videoFrames.forEach((frame) => {
-    input.input(frame);
-  });
 
-  input
-    .inputFormat('image2pipe')
-    .inputFPS(30)
-    .videoCodec('libx264')
-    .on('end', () => {
-      console.log('Decoding finished');
-      videoFrames = [];
-    })
-    .on('error', (err) => {
-      console.error('Error:', err);
-    })
-    .output('rtmp://a.rtmp.youtube.com/live2/YOUR_STREAM_KEY') // Replace with your YouTube Stream Key
-    .run();
+  try {
+    videoFrames.forEach((frame) => {
+      // Ensure frame is correctly formatted before adding as input
+      if (frame instanceof ArrayBuffer) {
+        const frameData = new Uint8Array(frame);
+        input.input(frameData);
+      } else {
+        console.error('Invalid frame format');
+      }
+    });
+
+    input
+      .inputFPS(30)
+      .videoCodec('libx264')
+      .on('end', () => {
+        console.log('Decoding finished');
+        videoFrames = [];
+        try {
+          console.log('Starting stream...');
+          startYouTubeAndTwitchStream();
+        } catch (error) {
+          console.error('Error starting the stream:', error);
+        }
+      })
+      .on('error', (err) => {
+        console.error('Error during decoding:', err);
+      })
+      .output('rtmp://a.rtmp.youtube.com/live2/YOUR_YOUTUBE_STREAM_KEY') // YouTube Stream Key
+      .output('rtmp://live.twitch.tv/app/live/YOUR_TWITCH_STREAM_KEY') // Twitch Stream Key
+      .run();
+  } catch (error) {
+    console.error('Error processing video frames:', error);
+  }
+}
+
+function isValidFrame(frame) {
+  // Implement validation for the frame format here
+  // You may need to check the frame data to ensure it's a valid format.
+  // Return true if it's valid, false otherwise.
+  return true; // Update this based on your frame format validation.
+}
+
+function startYouTubeAndTwitchStream() {
+  // Implement your stream start logic here
+  // This function will be called when decoding finishes
+  // and it's time to start streaming to YouTube and Twitch.
+  console.log('Starting YouTube and Twitch streams...');
 }
 
 server.listen(8080, () => {
